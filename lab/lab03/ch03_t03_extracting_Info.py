05e22157b1b37ac42ab65d4b90f0a1c0f6acae88
TsoiManHim2627 <gordontsoimh@gmail.com>
2026-09-15 02:41:21 +0000

    [skip actions] lab/lab03/ch03_t03_extracting_Info.py

--- lab/lab03/ch03_t03_extracting_Info.py
+++ lab/lab03/ch03_t03_extracting_Info.py
@@ -1,8 +1,5 @@
 from datetime import datetime
 now = datetime.now()
-current_year=now.year
-current_month=now.month
-current_day=now.day
-print(current_year)
-print(current_month)
-print(current_day)
+print(now.year)
+print(now.month)
+print(now.day)
