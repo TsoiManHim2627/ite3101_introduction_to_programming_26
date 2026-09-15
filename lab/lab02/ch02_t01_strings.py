8fd467d276c0187e8da16514120d0f8dd0418ca7
TsoiManHim2627 <gordontsoimh@gmail.com>
2026-09-15 02:11:55 +0000

    [skip actions] lab/lab02/ch02_t01_strings.py

--- lab/lab02/ch02_t01_strings.py
+++ lab/lab02/ch02_t01_strings.py
@@ -1,2 +1,10 @@
-#set the variable brian on line 3!
-brian ("hello life!"
+# set the variable brian on line 3!
+Brian = "Hello life!"
+name = "Ryan"
+age = "19"
+food = "cheese"
+
+print(brian)
+print(name)
+print(age)
+print(food)
