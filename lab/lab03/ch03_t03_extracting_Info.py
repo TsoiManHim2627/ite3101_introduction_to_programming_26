7203e6168c112797567c766ba86e1e498a019172
TsoiManHim2627 <gordontsoimh@gmail.com>
2026-09-15 02:37:36 +0000

    [skip actions] lab/lab03/ch03_t03_extracting_Info.py

--- lab/lab03/ch03_t03_extracting_Info.py
+++ lab/lab03/ch03_t03_extracting_Info.py
@@ -3,6 +3,6 @@ now = datetime.now()
 current_year=now.year
 current_month=now.month
 current_day=now.day
-print(now)
-print(now.month)
-print(now.day)
+print(current_year)
+print(current_month)
+print(current_day)
