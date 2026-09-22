a3f177e1add2b61eeab77aa87dd14c13f79ed687
TsoiManHim2627 <gordontsoimh@gmail.com>
2026-09-22 03:02:18 +0000

    [skip actions] lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -2,8 +2,8 @@ bool_one = False and False
 
 bool_two = -(-(-(-2)))==-2 and 4>=16**0.5
 
-bool_three = 19%4 !=
+bool_three = 19%4 != 300/10/10 and False
 
-bool_four = None
+bool_four = -(1**2)
 
 bool_five = None
